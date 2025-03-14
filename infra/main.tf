c92659491a7e5f0b4ec96a896f39f3f8a071847a
x034223 <hossein.hajjari@klm.com>
2025-03-14 09:54:29 +0100

    init

--- infra/main.tf
+++ infra/main.tf
@@ -58,3 +58,58 @@ resource "azurerm_role_assignment" "aci_acr_pull" {
   role_definition_name = "AcrPull"
   principal_id         = var.github_oidc_principal_id
 }
+
+resource "azurerm_api_management" "apim" {
+  name                = "apimproductservice"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  publisher_name      = "XYZ Group"
+  publisher_email     = "hajjari@gmail.com"
+  sku_name = "Developer_1" # Change based on your needs (Developer, Standard, Premium)
+  virtual_network_type = "None" # Set to "Internal" if using VNet
+}
+
+resource "azurerm_api_management_api" "product_api" {
+  name                = "product-api"
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+  revision           = "1"
+  display_name       = "Product API"
+  path              = "products"
+  protocols         = ["https"]
+
+  import {
+    content_format = "swagger-json"
+    content_value  = file("${path.module}/infra/swagger/product-service-swagger.json")
+  }
+}
+
+resource "azurerm_api_management_backend" "aci_backend" {
+  name                = "aci-backend"
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+  protocol           = "http"
+  url               = "http://xyz-product-service-1-0-0.westeurope.azurecontainer.io"
+}
+
+resource "azurerm_api_management_api_operation_policy" "set_backend_policy" {
+  api_name            = azurerm_api_management_api.product_api.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
+  operation_id        = "getAllProducts"
+
+  xml_content = <<XML
+  <policies>
+      <inbound>
+          <base />
+          <set-backend-service base-url="http://xyz-product-service-1-0-0.westeurope.azurecontainer.io" />
+      </inbound>
+      <backend>
+          <base />
+      </backend>
+      <outbound>
+          <base />
+      </outbound>
+  </policies>
+  XML
+}
